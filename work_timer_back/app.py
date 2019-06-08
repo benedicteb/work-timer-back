9e7aab3222ce5c8d463f058e5ba59766793a3f0b
Benedicte Emilie Brækken <b@brkn.io>
2019-06-08 17:01:13 +0200

    Add CORS

--- work_timer_back/app.py
+++ work_timer_back/app.py
@@ -4,3 +4,20 @@ from work_timer_back.json import ToJSONEncoder
 app = Flask("work-timer-back")
 
 app.json_encoder = ToJSONEncoder
+
+
+@app.after_request
+def apply_caching(response):
+    response.headers[
+        "Access-Control-Allow-Origin"
+    ] = "https://work-timer.benedicte.dev"
+
+    response.headers["Access-Control-Allow-Methods"] = ", ".join(
+        ["GET", "POST", "PUT", "OPTIONS"]
+    )
+
+    response.headers["Access-Control-Allow-Headers"] = ", ".join(
+        ["Authorization", "Content-Type"]
+    )
+
+    return response
